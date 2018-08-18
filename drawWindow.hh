4db11c4644e9c90e15bfad857cd89eb1be6a9ace
Pradu Kannan <pradu@pradu.us>
2018-08-18 16:24:24 -0700

    Add drawWindow and grid

---
+++ drawWindow.hh
@@ -0,0 +1,17 @@
+/*
+* drawWindow.hh
+* 
+* Copyright (c) 2018 Pradu Kannan. All rights reserved.
+*/
+
+#ifndef ___DRAWWINDOW_HH
+#define ___DRAWWINDOW_HH
+
+#include <cairo.h>
+
+struct DrawWindow
+{
+};
+
+#endif  // ___DRAWWINDOW_HH
+
---
+++ grid.hh
@@ -0,0 +1,26 @@
+/*
+* grid.hh
+* 
+* Copyright (c) 2018 Pradu Kannan. All rights reserved.
+*/
+
+#ifndef ___GRID_HH
+#define ___GRID_HH
+
+struct Node
+{
+    //type
+    int type;
+
+    //define geometry
+    double x;
+    double y;
+    double size;
+};
+
+struct Grid
+{
+};
+
+#endif  // ___GRID_HH
+
